f27115f4d7cf21f52cac147e95b6e785ecff7c04
Davide <davidevischetti@gmail.com>
2022-06-17 15:42:18 +0200

    import font awesome

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,15 @@
+<template>
+<div>ciao</div>
+</template>
+
+<script>
+export default {
+  name: 'MyHeader',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+
+// FONT AWESOME
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+library.add(faUserSecret)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+
 Vue.config.productionTip = false
 
 new Vue({
